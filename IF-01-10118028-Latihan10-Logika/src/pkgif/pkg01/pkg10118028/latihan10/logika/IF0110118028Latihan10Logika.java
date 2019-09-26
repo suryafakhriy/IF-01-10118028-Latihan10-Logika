f73a522615f42e5eb21127d73104d6b2a09b0b6c
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-09-26 21:40:08 +0700

    Add files via upload

---
+++ IF-01-10118028-Latihan10-Logika/src/pkgif/pkg01/pkg10118028/latihan10/logika/IF0110118028Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg01.pkg10118028.latihan10.logika;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : Logika
+ */
+public class IF0110118028Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
